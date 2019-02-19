a249c5511ff244843d0451147c4a84eab74c6415
SantiMizu <santivilamizu@gmail.com>
2019-02-19 21:23:16 +0100

    Nuevo commit?

--- app/src/main/java/com/example/vesprada/appdependencia/Activities/ConfiguracionActivity.kt
+++ app/src/main/java/com/example/vesprada/appdependencia/Activities/ConfiguracionActivity.kt
@@ -4,13 +4,13 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
-import android.os.AsyncTask
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.text.InputType
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -22,26 +22,27 @@ import com.example.vesprada.appdependencia.Utils.PdfFromXmlFile
 import kotlinx.android.synthetic.main.activity_configuracion.*
 import kotlinx.android.synthetic.main.app_bar_configuracion.*
 import kotlinx.android.synthetic.main.content_configuracion.*
-import java.io.OutputStream
-import java.net.HttpURLConnection
-import java.net.URL
 
 
 class ConfiguracionActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private val MYPREFS = "MyPrefs"
     private val DNI = "dni"
+    private val PASS = "passwd"
+    private val ASISTENTE = "ipasistente"
+
+    private var passwdVisible=false
 
     lateinit var sharedPreferences: SharedPreferences
 
     lateinit var btnSave : Button
     lateinit var btnCancel : Button
-    lateinit var etNombre: EditText
+    lateinit var etDNI: EditText
     lateinit var etPasswd: EditText
     lateinit var ipAsistente: EditText
-    lateinit var ultimoNombre : String
-    lateinit var ultimaPasswd : String
     lateinit var ultimaIpAsistente : String
+
+    var input_type : Int = 0
     var editando: Boolean = false
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -51,6 +52,7 @@ class ConfiguracionActivity : AppCompatActivity(), NavigationView.OnNavigationIt
                 var intent = Intent(this, RedButtonActivity::class.java)
                 startActivity(intent)
                 finish()
+
             }
 
             R.id.notificaciones -> {
@@ -101,9 +103,9 @@ class ConfiguracionActivity : AppCompatActivity(), NavigationView.OnNavigationIt
 
         nav_view.setNavigationItemSelectedListener(this)
 
-        setUI()
-
         sharedPreferences = getSharedPreferences(MYPREFS, Context.MODE_PRIVATE)
+
+        setUI()
 
         if(savedInstanceState!=null){
             restoreUI(savedInstanceState)
@@ -113,25 +115,22 @@ class ConfiguracionActivity : AppCompatActivity(), NavigationView.OnNavigationIt
     private fun setUI() {
         btnSave = findViewById(R.id.btn_save)
         btnCancel = findViewById(R.id.btn_cancelarCambios)
-        etNombre = findViewById(R.id.ed_nombreDependiente)
-        etPasswd = findViewById(R.id.ed_passwdDependiente)
+        etDNI = findViewById(R.id.ed_dniDependiente)
+        etPasswd = findViewById(R.id.ed_PasswdDependiente)
         ipAsistente = findViewById(R.id.ed_ipAsistente)
+
+        input_type = etPasswd.inputType
 
         //Carga el nombre, contraseña e ip del asistente que tenia previamente
         cargarDatosIniciales()
     }
 
     private fun restoreUI(bundle: Bundle){
-        etNombre.setText(bundle.getString("etNombreText"))
-        etPasswd.setText(bundle.getString("etPasswdText"))
+
         ipAsistente.setText(bundle.getString("ipAsistenteText"))
 
-        etNombre.isEnabled=bundle.getBoolean("etNombreEnable")
-        etPasswd.isEnabled=bundle.getBoolean("etPasswdEnable")
         ipAsistente.isEnabled=bundle.getBoolean("ipAsistenteEnable")
 
-        ultimoNombre = bundle.getString("ultimoNombre")
-        ultimaPasswd = bundle.getString("ultimaPasswd")
         ultimaIpAsistente = bundle.getString("ultimaIpAsistente")
 
         btnSave.visibility=bundle.getInt("botonesVisibles")
@@ -142,29 +141,27 @@ class ConfiguracionActivity : AppCompatActivity(), NavigationView.OnNavigationIt
     private fun cargarDatosIniciales() {
 
         //rellenar los campos con los de la base de datos
-        etNombre.setText("Nombre De Ejemplo")
-        etPasswd.setText("Contraseña De Ejemplo")
-        ipAsistente.setText("none")
+        etDNI.setText(sharedPreferences.getString(DNI, "00000000a"))
+        etPasswd.setText(sharedPreferences.getString(PASS, "1234"))
+        ipAsistente.setText(sharedPreferences.getString(ASISTENTE,""))
 
-        ultimoNombre=etNombre.text.toString()
-        ultimaPasswd=etPasswd.text.toString()
+        if(ipAsistente.text.toString().length==0){
+            btnSave.visibility=View.VISIBLE
+            Toast.makeText(this, getString(R.string.no_hay_ip_asistente),Toast.LENGTH_LONG).show()
+            ipAsistente.isEnabled=true
+        }
+
         ultimaIpAsistente=ipAsistente.text.toString()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
 
-        outState.putBoolean("etNombreEnable", etNombre.isEnabled)
-        outState.putBoolean("etPasswdEnable", etPasswd.isEnabled)
         outState.putBoolean("ipAsistenteEnable", ipAsistente.isEnabled)
 
         outState.putInt("botonesVisibles", btnSave.visibility)
 
-        outState.putString("etNombreText", etNombre.text.toString())
-        outState.putString("etPasswdText",etPasswd.text.toString())
         outState.putString("ipAsistenteText", ipAsistente.text.toString())
 
-        outState.putString("ultimoNombre", ultimoNombre)
-        outState.putString("ultimaPasswd", ultimaPasswd)
         outState.putString("ultimaIpAsistente",ultimaIpAsistente)
 
         super.onSaveInstanceState(outState)
@@ -173,27 +170,22 @@ class ConfiguracionActivity : AppCompatActivity(), NavigationView.OnNavigationIt
     //Guarda los cambios
     fun GuardarOnClick(v: View){
 
-        //Si se esta editando editando la ip del asistente...
-        if(etNombre.isEnabled){
-            //Aqui habría que poner que guarde los datos correspondientes
-            ultimoNombre=etNombre.text.toString()
-            ultimaPasswd=etPasswd.text.toString()
-        }
-        //Si se esta editando el nombre y contraseña...
-        else {
-            //Aqui habría que poner que guarde los datos correspondientes
-            ultimaIpAsistente=ipAsistente.text.toString()
+        //Aqui habría que poner que guarde los datos correspondientes
+        ultimaIpAsistente=ipAsistente.text.toString()
+        val editor = sharedPreferences.edit()
+        editor.putString(ASISTENTE, ipAsistente.text.toString())
+        editor.commit()
 
-        }
-
-        desacivarCampos()
+        ipAsistente.isEnabled = false
+        btnSave.visibility = View.GONE
 
     }
 
+    //------------------------------------
+    //Puede ser que esto haya que borrarlo
+    //------------------------------------
     fun CancelarOnClick(v: View){
 
-        etNombre.setText(ultimoNombre)
-        etPasswd.setText(ultimaPasswd)
         ipAsistente.setText(ultimaIpAsistente)
 
         desacivarCampos()
@@ -203,8 +195,6 @@ class ConfiguracionActivity : AppCompatActivity(), NavigationView.OnNavigationIt
     private fun desacivarCampos() {
         btnSave.visibility=View.GONE
         btnCancel.visibility=View.GONE
-        ed_nombreDependiente.isEnabled=false
-        ed_passwdDependiente.isEnabled=false
         ipAsistente.isEnabled=false
         editando=false
     }
@@ -239,19 +229,15 @@ class ConfiguracionActivity : AppCompatActivity(), NavigationView.OnNavigationIt
             R.id.nav_mod_datos->{
 
                 //comprueba que no le haya dado antes a editar
-                if(!ed_nombreDependiente.isEnabled) {
+                if(!passwdVisible) {
 
-                    //Muestra el dialogo que pide la contraseña del dependiente
-                    val fm = supportFragmentManager
-                    var dialog = DialogoPersonalizadoDependiente()
-
-                    //Para que active los campos de edición del dependiente hay que pasar un '1' como primer paramentro
-
-                    dialog.DependienteOAsistente(1, "1234")
-                    dialog.show(fm, "Patata")
+                    etPasswd.inputType =  InputType.TYPE_CLASS_TEXT
+                    passwdVisible = true
 
                 } else {
-                    Toast.makeText(this, getString(R.string.ya_esta_editando), Toast.LENGTH_LONG).show()
+
+                    etPasswd.inputType =  input_type
+
                 }
             }
             R.id.nav_mod_ip_asistente->{
